47b5974ba5e4a497703c9871b6fa5dbf02499873
Fritz Immanuel <fritz@atomic.id>
2025-05-26 11:17:28 +0700

    Update streamlit app

--- streamlit/streamlit_app.py
+++ streamlit/streamlit_app.py
@@ -33,17 +33,23 @@ if input_mode == 'Upload CSV file':
 	if uploaded_file:
 		df_input = pd.read_csv(uploaded_file)
 
+		# Drop 'churned' if it's included in the upload
+		if 'churned' in df_input.columns:
+			df_input = df_input.drop(columns='churned')
+
 		# Check and align columns
 		missing_cols = set(feature_columns) - set(df_input.columns)
 		extra_cols = set(df_input.columns) - set(feature_columns)
 
 		if missing_cols:
 			st.error(f"Missing columns: {missing_cols}")
-		elif extra_cols:
-			st.warning(f"Ignoring extra columns: {extra_cols}")
-			df_input = df_input[feature_columns]
 		else:
-			st.success("Input file validated. Ready to predict.")
+			if extra_cols:
+				st.warning(f"Ignoring extra columns: {extra_cols}")
+				df_input = df_input[feature_columns]
+			else:
+				st.success("Input file validated. Ready to predict.")
+
 			if st.button("Predict"):
 				y_pred, y_prob = predict(df_input)
 				df_input['churn_prediction'] = y_pred
@@ -57,13 +63,13 @@ else:
 
 	for col in feature_columns:
 		if 'review_pct_' in col or 'avg_' in col or 'std_' in col:
-			input_data[col] = st.number_input(f"{col}", value=feature_template[col].mean())
+			input_data[col] = st.number_input(f"{col}", value=round(feature_template[col].mean(), 2))
 		elif 'payment_type' in col:
 			input_data[col] = st.selectbox(f"{col}", [0, 1])
 		elif 'top_master_category' in col or 'cohort_month' in col:
 			input_data[col] = st.text_input(f"{col}")
 		else:
-			input_data[col] = st.number_input(f"{col}", value=feature_template[col].mean())
+			input_data[col] = st.number_input(f"{col}", value=round(feature_template[col].mean(), 2))
 
 	if st.button("Predict"):
 		df_manual = pd.DataFrame([input_data])
